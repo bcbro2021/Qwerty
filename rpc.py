9e0bf4173d68f8cb75ff48b142d771ccbedc7e63
Yohaan Renooj <yohaanrenooj2611@gmail.com>
2025-07-07 01:39:09 +0530

    Add files via upload

---
+++ rpc.py
@@ -0,0 +1,28 @@
+from pypresence import Presence
+from dotenv import load_dotenv
+import time, os
+
+rpc_enable = False
+
+if rpc_enable:
+    load_dotenv()
+
+    CLIENT_ID = os.getenv("DISCORD_CLIENT_ID")
+
+    if not CLIENT_ID:
+        raise ValueError("Discord Client ID is missing. Please set it in the .env file.")
+    rpc = Presence(CLIENT_ID)
+    rpc.connect()
+
+def update():
+    rpc.update(
+        state=f"Coding Away in Qwerty",
+        details=f"F off",
+        start=time.time(),  # Start time for session
+        large_image="logo",  # Must be uploaded in Discord Developer Portal
+        large_text="Qwerty"
+    )
+
+def close():
+    rpc.clear()
+    rpc.close()
